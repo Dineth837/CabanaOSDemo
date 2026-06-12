538234057744b85fc844940a7e7455b161b6ed07
Dineth837 <dineth396@gmail.com>
2026-06-12 12:18:37 +0530

    Welcome UI

---
+++ Views/DefaultWelcomeView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CabanaOSDemo.Views
+{
+    public partial class DefaultWelcomeView : UserControl
+    {
+        public DefaultWelcomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
